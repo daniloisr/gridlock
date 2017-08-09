3bf8651b48d62ad2e9798b57d3d7e618131dfaa7
Danilo Resende <danilo.isr@gmail.com>
2017-08-08 23:18:55 -0300

    Reset dancing_links to try a simpler solution

--- solvers/dancing_links/solver.rb
+++ solvers/dancing_links/solver.rb
@@ -1,213 +1,8 @@
-# require 'rotator'
 require 'byebug'
+require 'ostruct'
+require 'minitest'
 
-class DblList
-  include Enumerable
-
-  attr_accessor :val, :pred, :succ
-
-  def initialize(val = self)
-    @val = val
-    @pred = self
-    @succ = self
-  end
-
-  # @param item [DblList] the item to be added at end of this list
-  def add(item)
-    # current state: pred <-> self
-
-    # first step: update item refs
-    # pred <- item -> self
-    item.pred = pred
-    item.succ = self
-
-    # second step: update this list refs
-    # pred -> item <- self
-    pred.succ = item
-    self.pred = item
-  end
-
-  # @yield [DblList, Object] val of each node in the list
-  def each
-    p = self
-    loop do
-      # @todo yielding .val is a hack to help Node management,
-      #       move this to a better place
-      yield p.val
-      break if (p = p.succ) == self
-    end
-  end
-
-  # @param index [Integer]
-  # @return [DblList] element at
-  def [](index)
-    index.zero? ? val : succ[index - 1]
-  end
-end
-
-# Keep track of piece solution in the grid
-# It is used to remove piece's solutions from the grid
-class PieceSolution
-  attr_reader :piece, :solutions
-
-  def initialize(piece)
-    # @todo not sure @piece will be need
-    @piece = piece
-    @solutions = []
-  end
-
-  # Add a new PieceNode to the solutions array
-  #
-  # @param [PieceNode]
-  def <<(ref)
-    @solutions << ref
-  end
-end
-
-module Node
-  attr_reader :row, :column
-
-  def init_lists
-    @row = DblList.new(self)
-    @column = DblList.new(self)
-  end
-
-  # @todo add docs
-  def [](direction)
-    case direction
-    when :lt then @row.succ.val
-    when :rg then @row.pred.val
-    when :up then @column.pred.val
-    when :dn then @column.succ.val
-    else raise(ArgumentError, "direction #{direction} not supported")
-    end
-  end
-
-  # Add node to the bottom of column
-  #
-  # @param [Node]
-  def add_to_column(node)
-    @column.add(node.column)
-  end
-
-  # Add node to the end of row
-  #
-  # @param [Node]
-  def add_to_row(node)
-    @row.add(node.row)
-  end
-
-  def id
-    object_id.to_s(36)
-  end
-
-  def lists_refs
-    format(
-      'lt#%s rg#%s up#%s dw#%s',
-      *[lt, rg, up, dw].map(&:id)
-    )
-  end
-
-  def inspect
-    format('<#%s %s>', id, lists_refs)
-  end
-end
-
-class PieceNode
-  include Node
-
-  attr_reader :piece_solution, :head
-
-  def initialize(piece_solution, head)
-    @piece_solution = piece_solution
-    @head = head
-    init_lists
-
-    # link solution cell with the header row
-    head.add_to_column(self)
-  end
-end
-
-class HeaderCell
-  include Node
-
-  attr_reader :symbol, :size
-
-  def initialize(symbol)
-    @symbol = symbol
-    @size = 0
-    init_lists
-  end
-
-  # Override Node#[] to add the hability to fetch horizontal nodes by index
-  #
-  # @param [Number, Symbol] index or direction
-  # @return [HeaderCell]
-  def [](arg)
-    arg.class == Symbol ? super : @row[arg]
-  end
-
-  # Add node to the end of column and increment size
-  #
-  # @param (see Node#add_to_column)
-  def add_to_column(node)
-    super
-    @size += 1
-  end
-
-  def print_row(stop = nil)
-    return symbol.to_s if self[:rg] == stop
-    format('%s %s', symbol, self[:rg].print_row(stop ? stop : self))
-  end
-end
-
-# setup header
 def main
-  board = [2, 'oxxx']
-  header_ref, current_ref = nil
-
-  board[1].each_char do |symbol|
-    new_ref = HeaderCell.new(symbol)
-
-    current_ref&.add_to_row(new_ref)
-    current_ref = new_ref
-    header_ref = current_ref unless header_ref
-  end
-
-  pieces = [[2, 'xo'], [2, 'xx']]
-  piece_solutions = pieces.each_with_index.map do |piece, index|
-    # @todo generate board-fit for each piece...
-    hard_coded_fits =
-      case index
-      when 0
-        [[0, 1], [0, 2]]
-      when 1
-        [[1, 3], [2, 3]]
-      end
-
-    PieceSolution.new(piece).tap do |piece_solution|
-      # @todo move to a method
-      hard_coded_fits.each do |positions|
-        solution_node = nil
-
-        positions.each do |column_index|
-          # init new node for solution
-          new_solution_node = PieceNode.new(piece_solution, header_ref[column_index])
-          if solution_node
-            # add the new solution ref to the end of solution row
-            solution_node.add_to_row(new_solution_node)
-          else
-            # for the first node solution_node will be nil and we assign it
-            solution_node = new_solution_node unless solution_node
-          end
-        end
-
-        # piece_solution will point to all solution_nodes
-        piece_solution << solution_node
-      end
-    end
-  end
-
   # # Solution Matrix
   #
   #    O X X X HEADER
@@ -217,25 +12,107 @@ def main
   #    ------- XX piece
   #    . x . x
   #    . . x x
-  puts "#{header_ref.print_row.upcase} HEADER"
+  board = new_grid(<<~HEAD)
+    o x
+    x x
+  HEAD
 
-  piece_solution = piece_solutions.first
-  puts "------- #{piece_solution.piece[1].upcase} piece"
+  # header = setup_header(board)
+  # puts each_row(header).map { |p| p[:v] }.join(' ').upcase + ' HEADER'
 
-  heads = piece_solution.solutions.first.row.map(&:head)
-  puts header_ref.row.map {|i| heads.include?(i) ? 'x' : '.' }.join(' ')
+  piece_xo = new_grid('o x')
+  # piece_xx = new_grid('x x')
 
-  heads = piece_solution.solutions[1].row.map(&:head)
-  puts header_ref.row.map {|i| heads.include?(i) ? 'x' : '.' }.join(' ')
-
-  piece_solution = piece_solutions[1]
-  puts "------- #{piece_solution.piece[1].upcase} piece"
-
-  heads = piece_solution.solutions.first.row.map(&:head)
-  puts header_ref.row.map {|i| heads.include?(i) ? 'x' : '.' }.join(' ')
-
-  heads = piece_solution.solutions[1].row.map(&:head)
-  puts header_ref.row.map {|i| heads.include?(i) ? 'x' : '.' }.join(' ')
+  puts
 end
 
-main
+def setup_header(board)
+  head, *tail = board[:cells]
+  header = p = { v: head }
+  p[:r] = p[:l] = p
+  tmp = {}
+
+  tail.each do |cell|
+    tmp[:v]   = cell
+    tmp[:l]   = p
+    tmp[:r]   = p[:r]
+    p[:r][:l] = tmp
+    p[:r]     = tmp
+
+    p = tmp
+    tmp = {}
+  end
+
+  header
+end
+
+def rotate(x, y, rotation)
+  ((x + y * 1i) * 1i**rotation).rect
+end
+
+def find_matches(board, piece)
+  matches = []
+  board[:cells].size.times do |i|
+    piece[:cells].size.times do |j|
+      4.times do |rotation|
+        p = 0
+        s = []
+
+        loop do
+          jrx, jry = rotate(j / piece[:width], j % piece[:width], rotation)
+
+          break if board[:cells][ir] != piece[:cells][jr]
+          s << ir
+          p += 1
+
+          if p == piece[:cells].size
+            matches << s.sort
+            break
+          end
+        end
+      end
+    end
+  end
+
+  matches.uniq
+end
+
+def each_row(ref)
+  p = ref
+
+  Enumerator.new do |y|
+    loop do
+      y << p
+      p = p[:r]
+      break if p == ref
+    end
+  end
+end
+
+def new_grid(str)
+  lines = str.delete(' ').split
+  { width: lines.first.size, cells: lines.join.chars }
+end
+
+class SolverTest < Minitest::Test
+  def board
+    new_grid(<<~HEAD)
+      o x
+      x x
+    HEAD
+  end
+
+  def test_setup_header
+    header = setup_header(board)
+    vals = each_row(header).map { |cell| cell[:v] }
+    assert_equal 'oxxx', vals.join
+  end
+
+  def test_piece_matches
+    piece = new_grid('x o')
+    matches = find_matches(board, piece)
+    assert_equal [[0, 1], [0, 2]], matches
+  end
+end
+
+Minitest.autorun
