9500398a5eab858980910ce49f2142e0d0d45cba
Danilo Resende <danilo.isr@gmail.com>
2017-08-10 22:16:57 -0300

    Print the solution matrix

--- solvers/dancing_links/solver.rb
+++ solvers/dancing_links/solver.rb
@@ -3,6 +3,20 @@ require 'ostruct'
 require 'minitest'
 
 def main
+  board = new_grid(<<~HEAD)
+    o x
+    x x
+  HEAD
+
+  header = setup_header(board)
+  hrefs  = row_cells(header).to_a
+
+  # create linked list for 'piece' at 'matches' positions
+  piece_xo = new_grid('o x')
+  piece_xo_solutions = create_solutions(board, hrefs, piece_xo)
+  piece_xx = new_grid('x x')
+  piece_xx_solutions = create_solutions(board, hrefs, piece_xx)
+
   # # Solution Matrix
   #
   #    O X X X HEADER
@@ -12,25 +26,65 @@ def main
   #    ------- XX piece
   #    . x . x
   #    . . x x
-  board = new_grid(<<~HEAD)
-    o x
-    x x
-  HEAD
-
-  # header = setup_header(board)
-  # puts each_row(header).map { |p| p[:v] }.join(' ').upcase + ' HEADER'
-
-  piece_xo = new_grid('o x')
-  # piece_xx = new_grid('x x')
+  puts hrefs.map { |p| p[:v] }.join(' ').upcase + ' HEADER'
+  puts hrefs.map { '-' }.join('-') + ' XO piece'
+  piece_xo_solutions.each do |sol|
+    prefs = row_cells(sol).map { |r| r[:h] }
+    puts hrefs.map { |href| prefs.include?(href) ? 'x' : '.' }.join(' ')
+  end
+  puts hrefs.map { '-' }.join('-') + ' XX piece'
+  piece_xx_solutions.each do |sol|
+    prefs = row_cells(sol).map { |r| r[:h] }
+    puts hrefs.map { |href| prefs.include?(href) ? 'x' : '.' }.join(' ')
+  end
 
   puts
 end
 
+def create_solutions(board, hrefs, piece)
+  matches = find_matches(board, piece)
+
+  matches.each_with_object([]) do |match, solutions|
+    refs = match.map { create_ref }
+
+    # horizontal links
+    refs.each_cons(2) do |a, b|
+      b[:l]     = a
+      b[:r]     = a[:r]
+      a[:r][:l] = b
+      a[:r]     = b
+    end
+
+    # vertical links
+    match.each_with_index do |m, i|
+      a = hrefs[m]
+      b = refs[i]
+      a[:v] += 1
+      b[:h] = a
+
+      b[:u]     = a
+      b[:d]     = a[:d]
+      a[:d][:u] = b
+      a[:d]     = b
+    end
+
+    solutions << refs.first
+  end
+end
+
+def create_ref(initial = {})
+  hash = Hash.new do |h, k|
+    h[k] = h if %i[l u r d].include?(k)
+    h[k] = 0 if k == :v
+  end
+  hash.merge(initial)
+end
+
 def setup_header(board)
   head, *tail = board[:cells]
-  header = p = { v: head }
+  header = p = create_ref(v: head)
   p[:r] = p[:l] = p
-  tmp = {}
+  tmp = create_ref
 
   tail.each do |cell|
     tmp[:v]   = cell
@@ -40,35 +94,32 @@ def setup_header(board)
     p[:r]     = tmp
 
     p = tmp
-    tmp = {}
+    tmp = create_ref
   end
 
   header
 end
 
-def rotate(x, y, rotation)
-  ((x + y * 1i) * 1i**rotation).rect
-end
-
 def find_matches(board, piece)
   matches = []
   board[:cells].size.times do |i|
-    piece[:cells].size.times do |j|
-      4.times do |rotation|
-        p = 0
-        s = []
+    4.times do |rotation|
+      s = []
 
-        loop do
-          jrx, jry = rotate(j / piece[:width], j % piece[:width], rotation)
+      piece[:cells].size.times do |j|
+        jx, jy = ((j / piece[:width] + (j % piece[:width]) * 1i) * 1i**rotation).rect
+        ix, iy = [(i / board[:width]) + jx, (i % board[:width]) + jy]
+        i2 = ix * board[:width] + iy
+        break if ix < 0 || i2 >  board[:cells].size ||
+                 iy < 0 || iy >= board[:width]
 
-          break if board[:cells][ir] != piece[:cells][jr]
-          s << ir
-          p += 1
+        break if board[:cells][i2] != piece[:cells][j]
 
-          if p == piece[:cells].size
-            matches << s.sort
-            break
-          end
+        s << i2
+
+        if j == piece[:cells].size - 1
+          matches << s.sort
+          break
         end
       end
     end
@@ -77,7 +128,7 @@ def find_matches(board, piece)
   matches.uniq
 end
 
-def each_row(ref)
+def row_cells(ref)
   p = ref
 
   Enumerator.new do |y|
@@ -95,23 +146,29 @@ def new_grid(str)
 end
 
 class SolverTest < Minitest::Test
-  def board
-    new_grid(<<~HEAD)
+  def setup
+    @board = new_grid(<<~HEAD)
       o x
       x x
     HEAD
   end
 
   def test_setup_header
-    header = setup_header(board)
-    vals = each_row(header).map { |cell| cell[:v] }
+    header = setup_header(@board)
+    vals = row_cells(header).map { |cell| cell[:v] }
     assert_equal 'oxxx', vals.join
   end
 
-  def test_piece_matches
+  def test_piece_matches_1
     piece = new_grid('x o')
-    matches = find_matches(board, piece)
+    matches = find_matches(@board, piece)
     assert_equal [[0, 1], [0, 2]], matches
+  end
+
+  def test_piece_matches_2
+    piece = new_grid('x x')
+    matches = find_matches(@board, piece)
+    assert_equal [[1, 3], [2, 3]], matches
   end
 end
 
